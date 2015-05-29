1fe92e040143d0b6735c20713995576a00d87363
Tiny <zjutiny@gmail.com>
2015-05-29 16:16:41 +0800

    add redis and bcrypt for mint home page. user info will be stored in redis.

--- public/javascripts/mint/mint.js
+++ public/javascripts/mint/mint.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
+
 	$("#turnToRegister").click(function(){
 		$("#login").animate({left:"-100%", display:"none"},500);
-		$("#signin").animate({left:"0"},1000);
+		$("#signin").animate({top:"0"},500);
+        $("#signin").animate({scrollTop:$("#password2").offset().top},500);
+
 	});
 	$("#turnToLogin").click(function(){
-		$("#login").animate({left:"0", display:"true"},1000);
-		$("#signin").animate({left:"100%"},500);		
-	});
+		$("#login").animate({left:"0", display:"true"},500);
+		$("#signin").animate({top:"-100%"},500);
+    });
 
-	$("#aboutContent").load("./text/about/.txt");
+	//$("#aboutContent").load("./text/about/.txt");
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+//var redis = require('redis');
+//var bcrypt = require('bcrypt');
+//var db = redis.createClient();
+
+//db.on('error', function(err){
+//   console.log('Error ' + err);
+//});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TinyMint | Web Designer' });
 });
 
-router.get('/home', function(req, res, next) {
-    res.render('home', { title: 'TinyMint | Create Your Web' });
-});
+//router.submit('/', function(){
+//
+//});
+
+//router.get('/home', function(req, res, next) {
+//    res.render('home', { title: 'TinyMint | Create Your Web' });
+//});
 
 module.exports = router;
