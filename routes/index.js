b27e70c4ef02f80dfb827df636296283c0c7d823
Tiny <zjutiny@gmail.com>
2015-06-01 00:57:13 +0800

    try to add register module

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var User = require('../lib/user');
 var router = express.Router();
 //var redis = require('redis');
 //var bcrypt = require('bcrypt');
@@ -10,8 +11,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TinyMint | Web Designer' });
+    res.render('index', { title: 'TinyMint | Web Designer' });
 });
+
+exports.submit = function (req, res, next) {
+    var data = req.body.user;
+    User.getByName(data.name, function (err, user) {
+        if(err) return next(err);
+        if(user.id) {
+            res.error("Username already exists!");
+            res.redirect('back');
+        }else{
+            user = new User({
+                name: data.name,
+                pass: data.password,
+                mail: data.mail
+            });
+
+            user.save(function(err){
+                if(err) return next(err);
+                req.session.uid = user.id;
+                res.redirect('/');
+            })
+        }
+    })
+}
 
 //router.submit('/', function(){
 //
