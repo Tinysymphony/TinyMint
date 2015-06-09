701780effc9b37544798cda94b640b86cf41e6f4
Tiny <zjutiny@gmail.com>
2015-06-10 01:09:22 +0800

    append different div

--- public/javascripts/editor/plat.js
+++ public/javascripts/editor/plat.js
@@ -23,30 +23,52 @@ $(document).ready(function(){
         }
     });
 
-    $("#titleLine").click(function(){
-        segCount += 1;
-        var newSegment = "<div class='Segment' id='seg" + segCount + "'>Appended</div>";
-        var container = $("#segmentContainer");
-        var scrollTo = null;
-
-        if($(".Selected").length==0){
-            $("#segmentHolder").append(newSegment);
-            scrollTo = $("#segmentHolder").children(":last");
-        } else {
-            $(newSegment).insertAfter($(".Selected"));
-            scrollTo = $(".Selected").next();
-        }
-
-        var distance = scrollTo.offset().top - container.offset().top + container.scrollTop() - 80;
-        container.animate({scrollTop: distance}, 500);
-
-        setTimeout(setSelect(),0);
-        scrollTo.addClass("Selected");
-        scrollTo.css({"background-color":"#EEDC82"});
-        $(".Segment").not(scrollTo).removeClass("Selected");
-        $(".Segment").not(scrollTo).css({"background-color":"#fff"});
-
+    //-----Add Button Start-----//
+    $("#headLine").click(function(){
+        var newSegment = "<div class='Segment Seg-HeadLine' >Headline</div>";
+        appendSegment(newSegment);
     });
+
+    $("#paragraph").click(function(){
+        var newSegment = "<div class='Segment Seg-Paragraph' >Paragraph</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#article").click(function(){
+        var newSegment = "<div class='Segment Seg-Article' >Article</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#markdown").click(function(){
+        var newSegment = "<div class='Segment Seg-Markdown' >MD</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#gallery").click(function(){
+        var newSegment = "<div class='Segment Seg-Gallery' >Gallery</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#circles").click(function(){
+        var newSegment = "<div class='Segment Seg-Circles' >Circles</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#single").click(function(){
+        var newSegment = "<div class='Segment Seg-Single' >Single</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#music").click(function(){
+        var newSegment = "<div class='Segment Seg-Article' >Music</div>";
+        appendSegment(newSegment);
+    });
+
+    $("#video").click(function(){
+        var newSegment = "<div class='Segment Seg-Music' >Video</div>";
+        appendSegment(newSegment);
+    });
+    //-----Add Button End-----//
 
     $("#delete").click(function(){
         if($(".Selected").length!=0 && $(".Selected").attr("id")!="seg1"){
@@ -64,6 +86,31 @@ $(document).ready(function(){
 
 });
 
+function appendSegment(newSegment){
+    segCount += 1;
+    var segmentID = "seg" + segCount;
+    var container = $("#segmentContainer");
+    var scrollTo = null;
+
+    if($(".Selected").length==0){
+        $("#segmentHolder").append(newSegment);
+        scrollTo = $("#segmentHolder").children(":last");
+    } else {
+        $(newSegment).insertAfter($(".Selected"));
+        scrollTo = $(".Selected").next();
+    }
+
+    var distance = scrollTo.offset().top - container.offset().top + container.scrollTop() - 80;
+    container.animate({scrollTop: distance}, 500);
+
+    setTimeout(setSelect(),0);
+    scrollTo.attr("id", segmentID);
+    scrollTo.addClass("Selected");
+    scrollTo.css({"background-color":"#EEC900"});
+    $(".Segment").not(scrollTo).removeClass("Selected");
+    $(".Segment").not(scrollTo).css({"background-color":"#fff"});
+}
+
 function fold(){
     $("#editor").animate({left:'-30%'},"slow");
     $("#result").animate({width:'90%'},"slow");
@@ -77,7 +124,7 @@ function unfold(){
 function setSelect(){
     $(".Segment").click(function(){
         $(this).addClass("Selected");
-        $(this).css({"background-color":"#AB4082"});
+        $(this).css({"background-color":"#EEC900"});
         $(".Segment").not(this).removeClass("Selected");
         $(".Segment").not(this).css({"background-color":"#fff"});
     });
