6b7a229d81c2d03d47e5d7bec11c7d8ab323d511
Tiny <zjutiny@gmail.com>
2015-06-16 00:51:35 +0800

    approach to update result board

--- public/javascripts/editor/plat.js
+++ public/javascripts/editor/plat.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
 
     $("#article").click(function(){
         var newSegment = "" +
-            "<div class='Segment Seg-Paragraph row' >" +
+            "<div class='Segment Seg-Article row' >" +
             "<div class='col-md-12'>" +
             "<div class='fa fa-edit'> Article</div>" +
             "<hr class='featurette-divider' />" +
@@ -78,7 +78,7 @@ $(document).ready(function(){
 
     $("#markdown").click(function(){
         var newSegment = "" +
-            "<div class='Segment Seg-Paragraph row' >" +
+            "<div class='Segment Seg-Markdown row' >" +
             "<div class='col-md-12'>" +
             "<div class='fa fa-list-alt'> Markdown</div>" +
             "<hr class='featurette-divider' />" +
@@ -203,6 +203,10 @@ $(document).ready(function(){
 
     setSelect();
 
+    $("#refresh").click(function(){
+        loadMint();
+    });
+
 });
 
 function appendSegment(newSegment){
@@ -225,10 +229,8 @@ function appendSegment(newSegment){
     setTimeout(setSelect(),0);
     scrollTo.attr("id", segmentID);
     scrollTo.addClass("Selected");
-    //scrollTo.css({"background-color":"#EEC900"});
     scrollTo.css({"border":"8px solid #EEC900"});
     $(".Segment").not(scrollTo).removeClass("Selected");
-    //$(".Segment").not(scrollTo).css({"background-color":"#fff"});
     $(".Segment").not(scrollTo).css({"border":"none"});
 }
 
@@ -251,6 +253,84 @@ function setSelect(){
         //$(".Segment").not(this).css({"background-color":"#fff"});
         $(".Segment").not(this).css({"border":"none"});
     });
+}
+
+function loadMint(){
+
+    $("#resultBoard").empty();
+
+    var size = $(".Segment").length;
+
+    for(var i=0; i<size; i++){
+
+        var segment = $("#segmentHolder").children().eq(i);
+        var content;
+
+        if(segment.hasClass("DefaultTitle")){
+
+        }else if(segment.hasClass("Seg-Headline")){
+
+                content  = "<div class='jumbotron'><div class='container'>" +
+                "<h1>" +
+                segment.find("#headline-main-title").val() +
+                "</h1>" +
+                "<p>" +
+                segment.find("#headline-subtitle").val() +
+                "</p>" +
+                "</div></div>";
+
+        }else if(segment.hasClass("Seg-Paragraph")){
+
+            content = "<div class='container'>" +
+            "<p class='lead'>" +
+            segment.find("#paragraph-content").val() +
+            "</p>" +
+            "</div>";
+
+        }else if(segment.hasClass("Seg-Article")){
+
+            content = "<div class='container'><div class='row'>" +
+            "<div class='col-lg-12 '>" +
+            "<p>" +
+            segment.find("#article-content").val() +
+            "</p>" +
+            "</div>" +
+            "</div></div>";
+
+        }else if(segment.hasClass("Seg-Markdown")){
+
+        }else if(segment.hasClass("Seg-Single")){
+
+        }else if(segment.hasClass("Seg-Circles")){
+
+        }else if(segment.hasClass("Seg-Gallery")){
+
+            var id = segment.attr("id");
+
+            content = "<div class='carousel' data-ride='carousel'>" +
+            "<ol class='carousel-indicators'>" +
+            "<li data-target='#' data-slide-to='0'></li>" +
+            "<li data-target='#' data-slide-to='1' class='active'></li>" +
+            "<li data-target='#' data-slide-to='2'></li>" +
+            "</ol>" +
+            "</div>";
+
+        }else if(segment.hasClass("Seg-Music")){
+
+        }else if(segment.hasClass("Seg-Video")){
+
+        }
+
+        $("#resultBoard").append(content);
+
+        var newSegment = $("#resultBoard").children(":last");
+        newSegment.addClass("MintShow");
+        if(i%2)
+            newSegment.css("background-color", "#e4e4e4");
+        else
+            newSegment.css("background-color", "#fff");
+
+    }
 }
 
 $(window).resize(function(){
